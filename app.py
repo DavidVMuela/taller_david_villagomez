a4c6da5f224e306f1c37cf81289bc2a00aabe481
DavidVMuela <dom.villagomez@yavirac.edu.ec>
2025-11-19 16:54:26 -0500

    ssh

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "¡Hola Mundo desde Flask!"
+    return "Hello World from Flask"
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 80))
